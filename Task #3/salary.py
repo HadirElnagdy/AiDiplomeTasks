b541e009d9c78be5142cdf375f541fc03a9cd47d
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2021-08-06 19:27:30 +0200

    Uploaded from cmd

---
+++ Task #3/salary.py
@@ -0,0 +1,17 @@
+def addPercentage(sal, percent):
+    sal += (sal*percent/100)
+    return sal 
+    
+def managerSalary(oldSalary):
+    return 2*oldSalary
+
+def addBonus(mainSalary):
+    return mainSalary*10/100
+ 
+eidBonus = 500.0
+ramadanBonus = 200.0
+paidLeave = 15
+
+    
+    
+    
